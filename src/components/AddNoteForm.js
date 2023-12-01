09b2c7ffcc1afb285297406f7da7d6dddd21212a
andrew <csandrew@umich.edu>
2023-12-01 15:05:23 -0500

    Noter Project Frontend

--- src/components/AddNoteForm.js
+++ src/components/AddNoteForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './AddNoteForm.css';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const AddNoteForm = ({ addNote }) => {
   const [title, setTitle] = useState('');
@@ -20,28 +22,40 @@ const AddNoteForm = ({ addNote }) => {
   };
 
   return (
-    <form id="AddNoteForm" onSubmit={handleSubmit}>
-      <label>
-        <input
-          id='note-title'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter note name..."
-        />
-      </label>
-      <br />
-      <label>
-        <textarea
-          id='note-content'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter notes..."
-          rows="5" // Adjust the number of rows as needed
-        />
-      </label>
-      <br />
-      <button type="submit">Add</button>
-    </form>
+    <div id="new-note-container">
+      <form className="note-container-item" id="AddNoteForm" onSubmit={handleSubmit}>
+        <label id="title-container">
+          <input
+            id='note-title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter note name..."
+          />
+        </label>
+        <br />
+        <label id="note-container">
+          <textarea
+            id='note-content'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter notes..."
+            rows="5"
+          />
+        </label>
+        <label id="note-button">
+          <button type="submit">Add</button>
+        </label>
+        <br />
+      </form>
+      <div className="note-container-item" id="note-preview">
+        <h2 id="preview-notice">Note preview:</h2>
+        <div id="preview-content">
+          <ReactMarkdown remarkPlugins={[remarkGfm]} >{title}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[remarkGfm]} >{text}</ReactMarkdown>
+        </div>
+      </div>
+    </div>
+    
   );
 };
 
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Note.css';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const Note = ({ note, deleteNote }) => {
   return (
     <div className="note">
       <li>
-        <h4>{note.title}</h4>
-        <p>{note.text}</p>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{note.title}</ReactMarkdown>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{note.text}</ReactMarkdown>
         <button onClick={() => deleteNote(note._id)}>Delete</button>
       </li>
     </div>
